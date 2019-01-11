720686fa33a9f7567b7894fc1bf6558bc95734e5
ZCDEV <zcdev@calvisitor-10-105-174-222.calvisitor.1918.berkeley.edu>
2019-01-11 14:54:58 -0800

    Make handlebar helper to manage view data display and fix moment.js console error

--- public/js/herd.js
+++ public/js/herd.js
@@ -25,4 +25,5 @@ $(document).ready(function() {
 
     var dateTime = $("#date-time").val();
     dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
 });
--- seeds.sql
+++ seeds.sql
@@ -4,6 +4,6 @@ INSERT INTO Communities (name, description, createdAt, updatedAt) VALUES
     ("UCSF", "Med students united", "2019-01-08 03:14:07", "2019-01-08 03:14:07");
 
 INSERT INTO Events (name, date, description, max_attendees, createdAt, updatedAt, CommunityId) VALUES
-	("UCB code event", "2019-04-09 16:20:00", "Coding together", 8, "2019-01-08 03:14:07", "2019-01-08 03:14:07", 1),
+	("UCB code event", "2019-04-09 16:20:00", "Coding together Coding together Coding together Coding together Coding together", 8, "2019-01-08 03:14:07", "2019-01-08 03:14:07", 1),
     ("Stanford spring event", "2019-05-03 18:00:00", "Spring fling party", 4, "2019-01-08 03:14:07", "2019-01-08 03:14:07", 2),
     ("UCSF polar bear swim", "2019-06-08 08:30:00", "Swim in the bay", 6, "2019-01-08 03:14:07", "2019-01-08 03:14:07", 3);
