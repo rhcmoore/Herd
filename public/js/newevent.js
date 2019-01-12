3f0988f242b46b9a18a2de7a295687a6219c7c99
ZCDEV <zcdev@Orange.attlocal.net>
2019-01-11 19:24:56 -0800

    Make default date tomorrow as min date

--- public/js/newevent.js
+++ public/js/newevent.js
@@ -32,17 +32,19 @@ $(document).ready(function() {
     }
 
     var date = new Date();
-    var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    var tomorrow = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1);
     
     $.datetimepicker.setLocale('en');
     
     $("#date").datetimepicker({
-    startDate: today,
-    minDate : 0,
+    startDate: tomorrow,
+    minDate : tomorrow,
     dayOfWeekStart : 1,
     lang: 'en',
-    value: today + "0:00",
+    value: tomorrow,
     step: 30
     });
+
+    $('#datetimepicker').datepicker('setDate', tomorrow);
 
 });
